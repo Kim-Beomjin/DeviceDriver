b194ee124a5f5c88448ed68c429e0bd5167d575e
beom-jin.kim <beom-jin.kim@samsung.com>
2025-07-24 15:15:41 +0900

    [feature] Add MockFlashMemoryDevice

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,10 +1,25 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
+using namespace testing;
+
+class MockFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long), (override));
+	MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
+class DeviceDriverFixture : public Test {
+public:
+	NiceMock<MockFlashMemoryDevice> mockHardware;
+	FlashMemoryDevice* hardware = &mockHardware;
 	DeviceDriver driver{ hardware };
+};
+
+TEST_F(DeviceDriverFixture, ReadSuccess) {
+	EXPECT_CALL(mockHardware, read)
+		.WillRepeatedly(Return(0));
+
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
 }
