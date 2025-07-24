a92cbf73d667b7b78784f9624306716478481092
beom-jin.kim <beom-jin.kim@samsung.com>
2025-07-24 16:04:57 +0900

    [Refactoring] Extract EXPECT_CALLs

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <string>
+#include <vector>
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
@@ -15,36 +16,50 @@ public:
 
 class DeviceDriverFixture : public Test {
 public:
+	void setNormalReadCall(int expected) {
+		EXPECT_CALL(mockHardware, read)
+			.WillRepeatedly(Return(expected));
+	}
+
+	void setNormalReadCallWithTimes(int expected, int times) {
+		EXPECT_CALL(mockHardware, read)
+			.Times(times)
+			.WillRepeatedly(Return(expected));
+	}
+
+	void setReadCalls(vector<int>& expected) {
+		EXPECT_CALL(mockHardware, read)
+			.WillOnce(Return(expected[0]))
+			.WillOnce(Return(expected[1]))
+			.WillOnce(Return(expected[2]))
+			.WillOnce(Return(expected[3]))
+			.WillOnce(Return(expected[4]))
+			.WillRepeatedly(Return(expected[5]));
+	}
+
 	NiceMock<MockFlashMemoryDevice> mockHardware;
 	FlashMemoryDevice* hardware = &mockHardware;
 	DeviceDriver driver{ hardware };
 };
 
 TEST_F(DeviceDriverFixture, ReadSuccess) {
-	EXPECT_CALL(mockHardware, read)
-		.WillRepeatedly(Return(0));
+	setNormalReadCall(0);
 
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
 }
 
+
 TEST_F(DeviceDriverFixture, ReadFiveTimes) {
-	EXPECT_CALL(mockHardware, read)
-		.Times(5)
-		.WillRepeatedly(Return(0));
+	setNormalReadCallWithTimes(0, 5);
 
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
 }
 
 TEST_F(DeviceDriverFixture, ReadException) {
-	EXPECT_CALL(mockHardware, read)
-		.WillOnce(Return(0))
-		.WillOnce(Return(0))
-		.WillOnce(Return(0))
-		.WillOnce(Return(0))
-		.WillOnce(Return(1))
-		.WillRepeatedly(Return(0));
+	vector<int> mockCalls = { 0, 0, 0, 0, 1, 0 };
+	setReadCalls(mockCalls);
 
 	try {
 		int data = driver.read(0xFF);
@@ -54,6 +69,15 @@ TEST_F(DeviceDriverFixture, ReadException) {
 		EXPECT_EQ(string{ e.what() },
 			string{ "Read Fail" });
 	}
+}
+
+TEST_F(DeviceDriverFixture, ReadErrorAfterFive) {
+	vector<int> mockCalls = { 0, 0, 0, 0, 0, 1 };
+	setReadCalls(mockCalls);
+
+	int data = driver.read(0xFF);
+	EXPECT_EQ(0, data);
+
 }
 
 int main() {
